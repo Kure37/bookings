ffdeef84c854d01a49c4830d41171af2a25aa12f
Kure <yui.kure@mmmcorp.co.jp>
2026-02-14 16:03:50 +0900

    Uploading some image and make routers.go to hosting static contents

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,11 +12,17 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
+
 	mux.Use(Nosurf)
 	mux.Use(middleware.Recoverer)
 	mux.Use(SessionLoad)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+
 	return mux
 
 }
